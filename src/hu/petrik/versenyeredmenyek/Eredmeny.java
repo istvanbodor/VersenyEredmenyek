e61a69934861eb95eb88d5d7114e3e134d461a20
Diak <info@petrik.hu>
2022-10-20 10:42:38 +0200

    Initial commit

---
+++ src/hu/petrik/versenyeredmenyek/Eredmeny.java
@@ -0,0 +1,37 @@
+package hu.petrik.versenyeredmenyek;
+
+import javax.print.attribute.IntegerSyntax;
+
+public class Eredmeny implements  Comparable<Eredmeny>{
+    private String reszIdo;
+    private String nev;
+
+    public Eredmeny(String reszIdo, String nev) {
+        this.reszIdo = reszIdo;
+        this.nev = nev;
+    }
+
+    public String getReszIdo() {
+        return reszIdo;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    @Override
+    public int compareTo(Eredmeny other) {
+      String[] thisReszidok =  this.reszIdo.split(":");
+      String[] otherReszidok =  other.reszIdo.split(":");
+      Integer thisPerc = Integer.parseInt(thisReszidok[0]);
+      Integer thisMasodperc = Integer.parseInt(thisReszidok[1]);
+      Integer otherPerc = Integer.parseInt(otherReszidok[0]);
+      Integer otherMasodperc = Integer.parseInt(otherReszidok[1]);
+
+      if (thisPerc.compareTo(otherPerc) ==0) {
+          return thisMasodperc.compareTo(otherMasodperc);
+      }
+
+        return thisPerc.compareTo(otherPerc);
+    }
+}
